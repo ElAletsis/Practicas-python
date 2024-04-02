735dcc40fd05b4f192b7c8f5537e6bd2ab4340b9
ElAletsis <163987425+ElAletsis@users.noreply.github.com>
2024-04-02 15:28:00 -0600

    Update __init__.py

--- Flask/app/__init__.py
+++ Flask/app/__init__.py
@@ -29,7 +29,7 @@ app.config['MAIL_SERVER'] = 'smtp-relay.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'alexis.correo.de.prueba@gmail.com'
-app.config['MAIL_PASSWORD'] = '#Passworddeprueba2024#'
+app.config['MAIL_PASSWORD'] = 'Passworddeprueba2024'
 mail = Mail(app)
 
 db = SQLAlchemy(app)
